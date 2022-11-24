1f932d8c95bbf44bfa353aea5a7f34731f751e13
nikit <nikita.nikita@gmail.com>
2022-11-24 10:20:51 +0530

    Write a Stopwatch Program for measuring the time that elapses between the start and end clicks

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -10,7 +10,7 @@ namespace LogicalProgram
     {
         static void Main(string[] args)
         {
-            ReverseNumber.Reverse();
+            StopWatch.watch();
             Console.ReadLine();
         }
     }
