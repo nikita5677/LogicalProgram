5eec43f8cd881ec6d733a24e4ad2fd9b6007c312
nikit <nikita.nikita@gmail.com>
2022-11-24 09:51:45 +0530

    Add project files.

---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FibbonnaciSeries.fibonnaci();
+            //Console.ReadLine();
+
+            //CuponNumber.cupon();
+            //Console.ReadLine();
+
+            //PerfectNumber.Perfect();
+            //Console.ReadLine();
+
+            //PrimeNumber.Prime();
+            //Console.ReadLine();
+
+            //ReverseNumber.Reverse();
+            //Console.ReadLine();
+
+            //StopWatch.watch();
+            //Console.ReadLine();
+        }
+    }
+}
