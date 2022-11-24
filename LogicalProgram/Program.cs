b76a28b7203cb6dbb178832404ac0485b63ff6a0
nikit <nikita.nikita@gmail.com>
2022-11-24 10:18:52 +0530

    Reverse the number

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -10,7 +10,7 @@ namespace LogicalProgram
     {
         static void Main(string[] args)
         {
-            PrimeNumber.Prime();
+            ReverseNumber.Reverse();
             Console.ReadLine();
         }
     }
