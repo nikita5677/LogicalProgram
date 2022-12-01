56cf12bd70c6ca6b207d99c3460bc19aefb3d3ae
nikita5677 <67727961+nikita5677@users.noreply.github.com>
2022-12-01 15:05:28 +0530

    Coupon Numbers

--- LogicalProgram/CuponNumber.cs
+++ LogicalProgram/CuponNumber.cs
@@ -8,12 +8,12 @@ namespace LogicalProgram
 {
     class CuponNumber
     {
+        //Created a Method Cupon
         public static void cupon()
         {
             var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             var stringChars = new char[6];
             var random = new Random();
-
             for (int i = 0; i < stringChars.Length; i++)
             {
                 stringChars[i] = chars[random.Next(chars.Length)];
